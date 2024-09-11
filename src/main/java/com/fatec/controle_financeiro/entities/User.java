3369c0d64aab704a7887278dd370b65320865e10
h2danilo <h2danilo@hotmail.com>
2024-09-11 19:26:50 -0300

    aula 11-09

--- src/main/java/com/fatec/controle_financeiro/entities/User.java
+++ src/main/java/com/fatec/controle_financeiro/entities/User.java
@@ -1,7 +1,17 @@
 package com.fatec.controle_financeiro.entities;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 public class User {
+    //Desabilitar qdo quiser trabalhar com @Valid
+    //@NotBlank(message = "O nome não pode estar em branco.")
     private String name;
+
+    //Desabilitar qdo quiser trabalhar com @Valid
+    //@NotNull(message = "A idade é obrigatória.")
+    //@Min(value = 0, message = "A idade não pode ser negativa.")
     private int age;
 
     // Construtor padrão
