724b51988cdd4c5ca764454d2181d7ae4855390d
h2danilo <h2danilo@hotmail.com>
2024-09-18 13:31:10 -0300

    add WebConfig

---
+++ src/main/java/com/fatec/controle_financeiro/config/WebConfig.java
@@ -0,0 +1,17 @@
+package com.fatec.controle_financeiro.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*") // Permite todas as origens
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
